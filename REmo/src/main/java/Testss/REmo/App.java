f15345bb07288173a45e905fafdb3e86af10bf64
hp <hp@dev-rama-pc>
2018-03-22 15:00:09 +0530

    entototo

---
+++ REmo/src/main/java/Testss/REmo/App.java
@@ -0,0 +1,13 @@
+package Testss.REmo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
